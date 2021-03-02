6a0fb74365991e762fda78be71da15bacb89943a
jefferson.souza <jefferson.souza@synchro.com.br>
2021-03-02 14:29:23 -0300

    refactoring project to use data from graphql endpoint

--- landing-page/src/types/api.ts
+++ landing-page/src/types/api.ts
@@ -1,8 +1,123 @@
-export type LogoProps = {
-  alternativeText: string
+export type TechIcon = {
+  icon: {
+    url: string
+  }
+  title: string
+}
+
+export type Module = {
+  title: string
+  subtitle: string
+  description: string
+}
+
+export type SocialLink = {
+  title: string
   url: string
+}
+
+export type Image = {
+  url: string
+  alternativeText: string
+}
+
+export type Author = {
+  name: string
+  role: string
+  description: string
+  socialLinks: SocialLink[]
+  photo: Image
+}
+
+export type Review = {
+  name: string
+  text: string
+  photo: {
+    url: string
+  }
+}
+
+export type Question = {
+  question: string
+  answer: string
+}
+
+export type LogoProps = Image
+
+export type HeaderProps = {
+  title: string
+  description: string
+  button: {
+    label: string
+    url: string
+  }
+  image: Image
+}
+
+export type SectionAboutProjectProps = {
+  title: string
+  description: string
+  image: Image
+}
+
+export type SectionTechProps = {
+  title: string
+  techIcons: TechIcon[]
+}
+
+export type SectionConceptsProps = {
+  title: string
+  concepts: Array<{
+    title: string
+  }>
+}
+
+export type SectionModulesProps = {
+  title: string
+  modules: Module[]
+}
+
+export type SectionAgendaProps = {
+  title: string
+  description: string
+}
+
+export type PricingBoxProps = {
+  totalPrice: number
+  numberInstallments: number
+  priceInstallment: number
+  benefits: string
+  button: {
+    label: string
+    url: string
+  }
+}
+
+export type SectionAboutUsProps = {
+  title: string
+  authors: Author[]
+}
+
+export type SectionReviewsProps = {
+  title: string
+  reviews: Review[]
+}
+
+export type SectionFaqProps = {
+  title: string
+  questions: Question[]
 }
 
 export type LandingPageProps = {
   logo: LogoProps
+  header: HeaderProps
+  sectionAboutProject: SectionAboutProjectProps
+  sectionTech: SectionTechProps
+  sectionConcepts: SectionConceptsProps
+  sectionModules: SectionModulesProps
+  sectionAgenda: SectionAgendaProps
+  pricingBox: PricingBoxProps
+  sectionAboutUs: SectionAboutUsProps
+  sectionReviews: SectionReviewsProps
+  sectionFaq: SectionFaqProps
 }
